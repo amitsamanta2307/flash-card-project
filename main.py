4299a40a201601c3840cba408a7eb30d087c1bc9
Amit Samanta <developer.amitsamanta@gmail.com>
2021-08-06 11:24:40 +0530

    Add flip card functionality

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("./data/french_words.csv")
 to_learn = data.to_dict(orient='records')
+current_card = {}
 
 
-# ------------------------ Generate Random Words ----------------------------#
+# -------------------------------- Methods -----------------------------------#
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
 
 
 # --------------------------------- UI -------------------------------------- #
@@ -20,10 +32,13 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 # Canvas
 canvas = Canvas(height=526, width=800)
 card_front_image = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_back_image = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_image)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
