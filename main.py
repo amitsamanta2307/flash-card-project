a277b235773744ae5ca3c0308bcf592da02edcec
Amit Samanta <developer.amitsamanta@gmail.com>
2021-08-06 10:57:00 +0530

    Create new flash card

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv("./data/french_words.csv")
+to_learn = data.to_dict(orient='records')
 
-# --------------------------------- UI --------------------------------------- #
+
+# ------------------------ Generate Random Words ----------------------------#
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
+
+# --------------------------------- UI -------------------------------------- #
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -12,18 +24,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(height=526, width=800)
 card_front_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front_image)
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="trouve", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
 cross_image = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=2)
 
 check_image = PhotoImage(file="./images/right.png")
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=2)
+
+next_card()
 
 window.mainloop()
